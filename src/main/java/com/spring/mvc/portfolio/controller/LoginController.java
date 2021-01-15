16b39ce451a7afea0e90cc05a5da816dbb0126bc
foxlo <foxlo@DESKTOP-43M7EB4>
2021-01-15 12:18:39 +0800

    SpringData - portfolio

--- src/main/java/com/spring/mvc/portfolio/controller/LoginController.java
+++ src/main/java/com/spring/mvc/portfolio/controller/LoginController.java
@@ -3,14 +3,30 @@ package com.spring.mvc.portfolio.controller;
 
 import com.spring.mvc.portfolio.entities.Investor;
 import com.spring.mvc.portfolio.service.PortfolioService;
+import java.util.Optional;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@Controller
+@RequestMapping("/portfolio")
 public class LoginController {
-
-    
+    @Autowired
+    PortfolioService portfolioService;
+    @RequestMapping(value = "/login")
+    public String login(
+            @RequestParam Optional<String> username,
+            @RequestParam Optional<String> password,
+            HttpSession httpSession){
+        Investor investor = portfolioService.getInvestorRepository().getInvestor(username.get());
+        if(investor != null && investor.getCode().equals(investor.getPassword())){
+            httpSession.setAttribute("investor", investor);
+            httpSession.setAttribute("watch_id", investor.getWatchs().iterator().next().getId());
+        }
+        return "redirect:/portfolio/index.jsp";
+    }
 }
--- src/main/java/com/spring/mvc/portfolio/controller/VerifyController.java
+++ src/main/java/com/spring/mvc/portfolio/controller/VerifyController.java
@@ -2,6 +2,7 @@ package com.spring.mvc.portfolio.controller;
 
 import com.spring.mvc.portfolio.entities.Investor;
 import com.spring.mvc.portfolio.repository.InvestorRepository;
+import com.spring.mvc.portfolio.service.PortfolioService;
 import java.util.Optional;
 import javax.servlet.http.HttpSession;
 import javax.transaction.Transactional;
@@ -12,6 +13,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping(value = "/portfolio/verify")
 public class VerifyController {
-    
+    @Autowired
+    PortfolioService portfolioService;
+    @Transactional
+    @RequestMapping(value = "/{id}/{code}")
+    public String verify(
+            @PathVariable("id") Optional<Integer> id,
+            @PathVariable("code") Optional<String> code,
+            HttpSession httpSession){
+        String message = "FAIL";
+        Investor investor = portfolioService.getInvestorRepository().findOne(id.get());
+        if(investor != null && investor.getCode().equals(code.get())){
+            portfolioService.getInvestorRepository().updatePass(id.get(), Boolean.TRUE);
+            message = "SUCCESS";
+        }
+        httpSession.setAttribute("message", message);
+
+        return "redirect:/portfolio/verify.jsp";
+    }
 }
